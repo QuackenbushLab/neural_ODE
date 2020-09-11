7ab91d1366cb43616b2f92efc3ec13b538390b32
Intekhab Hossain <ihossain@g.harvard.edu>
2020-09-11 02:49:51 +0000

    150 g, 8s, expl = T, bat = single (need improve)

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -113,7 +113,7 @@ def save_model(odenet, folder, filename):
 
 parser = argparse.ArgumentParser('Testing')
 parser.add_argument('--settings', type=str, default='config_inte.cfg')
-clean_name = "simulated_expression_chalmers_150genes_6samples"
+clean_name = "simulated_expression_chalmers_150genes_8samples"
 #parser.add_argument('--data', type=str, default='C:/STUDIES/RESEARCH/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 parser.add_argument('--data', type=str, default='/home/ubuntu/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 args = parser.parse_args()
