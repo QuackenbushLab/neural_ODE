c87f94b72f0faa838b9e8e259f01eb969c5887fb
Intekhab Hossain <ihossain@g.harvard.edu>
2020-10-09 07:39:58 +0000

    set up for first noise experiment

--- ode_net/code/extra_modules/train_inte_old.py
+++ ode_net/code/extra_modules/train_inte_old.py
@@ -117,8 +117,8 @@ def save_model(odenet, folder, filename):
 parser = argparse.ArgumentParser('Testing')
 parser.add_argument('--settings', type=str, default='config_inte.cfg')
 clean_name = "simulated_expression_chalmers_30genes_6samples_0noise"
-parser.add_argument('--data', type=str, default='C:/STUDIES/RESEARCH/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
-#parser.add_argument('--data', type=str, default='/home/ubuntu/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
+#parser.add_argument('--data', type=str, default='C:/STUDIES/RESEARCH/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
+parser.add_argument('--data', type=str, default='/home/ubuntu/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 args = parser.parse_args()
 
 # Main function
@@ -164,8 +164,7 @@ if __name__ == "__main__":
     
     data_handler = DataHandler.fromcsv(args.data, device, settings['val_split'], normalize=settings['normalize_data'], 
                                         batch_type=settings['batch_type'], batch_time=settings['batch_time'], 
-                                        batch_time_frac=settings['batch_time_frac'],
-                                        noise = settings['noise'])
+                                        batch_time_frac=settings['batch_time_frac'])
 
     
     # Initialization
