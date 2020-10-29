619709307905c629a8f10108c7764af1e39c8f65
Intekhab Hossain <ihossain@g.harvard.edu>
2020-10-29 18:07:26 +0000

    sparse time experiments

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -114,7 +114,7 @@ def save_model(odenet, folder, filename):
 
 parser = argparse.ArgumentParser('Testing')
 parser.add_argument('--settings', type=str, default='config_inte.cfg')
-clean_name = "chalmers_150genes_50samples_earlyT_0noise"
+clean_name = "chalmers_30genes_50samples_0noise"
 #parser.add_argument('--data', type=str, default='C:/STUDIES/RESEARCH/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 parser.add_argument('--data', type=str, default='/home/ubuntu/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 
