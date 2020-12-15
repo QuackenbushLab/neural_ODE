ad2ee75dbe6aba1911979b96b7bfe7781bd2004e
Intekhab Hossain <ihossain@g.harvard.edu>
2020-12-15 03:45:27 +0000

    more 150 gene experiments

--- ode_net/code/odenet.py
+++ ode_net/code/odenet.py
@@ -39,12 +39,12 @@ class ODENet(nn.Module):
                 nn.LeakyReLU(),
                 nn.Linear(neurons, neurons),
                 nn.LeakyReLU(),
-                nn.Linear(neurons, neurons),
-                nn.LeakyReLU(),
-                nn.Linear(neurons, neurons),
-                nn.LeakyReLU(),
-                nn.Linear(neurons, neurons),
-                nn.LeakyReLU(),
+             #   nn.Linear(neurons, neurons),
+             #   nn.LeakyReLU(),
+             #   nn.Linear(neurons, neurons),
+             #   nn.LeakyReLU(),
+             #   nn.Linear(neurons, neurons),
+             #   nn.LeakyReLU(),
                 nn.Linear(neurons, ndim)
             )
 
