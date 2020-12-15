474c123dc0212852c7c0405d2d633d4641b9f750
Intekhab Hossain <ihossain@g.harvard.edu>
2020-12-15 15:09:15 +0000

    600 neurons. still need to reduce tru_mu loss

--- ode_net/code/odenet.py
+++ ode_net/code/odenet.py
@@ -39,12 +39,6 @@ class ODENet(nn.Module):
                 nn.LeakyReLU(),
                 nn.Linear(neurons, neurons),
                 nn.LeakyReLU(),
-             #   nn.Linear(neurons, neurons),
-             #   nn.LeakyReLU(),
-             #   nn.Linear(neurons, neurons),
-             #   nn.LeakyReLU(),
-             #   nn.Linear(neurons, neurons),
-             #   nn.LeakyReLU(),
                 nn.Linear(neurons, ndim)
             )
 
