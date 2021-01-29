de61c1bfbf74861072eb703f2b7a026908d824e3
Intekhab Hossain <ihossain@g.harvard.edu>
2021-01-29 01:04:59 -0500

    updated to plain L2 norm loss

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -72,7 +72,7 @@ def validation(odenet, data_handler, method, explicit_time):
             #predictions[index, :, :] = odeint(odenet, batch_point[0], time, method=method)[1:]
 
         # Calculate validation loss
-        loss = torch.mean((torch.abs(predictions - target) ** 1))
+        loss = torch.mean((predictions - target) ** 2)
     return [loss, n_val]
 
 def true_loss(odenet, data_handler, method):
@@ -83,7 +83,7 @@ def true_loss(odenet, data_handler, method):
             predictions[index, :, :] = odeint(odenet, batch_point, time, method=method)[1] #IH comment
         
         # Calculate true mean loss
-        loss = torch.mean((torch.abs(predictions - target) ** 1))
+        loss = torch.mean((predictions - target) ** 2)
     return loss
 
 
@@ -103,7 +103,7 @@ def training_step(odenet, data_handler, opt, method, batch_size, explicit_time, 
     predictions = torch.zeros(batch.shape).to(data_handler.device)
     for index, (time, batch_point) in enumerate(zip(t, batch)):
         predictions[index, :, :] = odeint(odenet, batch_point, time, method=method)[1] #IH comment
-    loss = torch.mean((torch.abs(predictions - target) ** 1))
+    loss = torch.mean((predictions - target) ** 2)
     loss.backward() #MOST EXPENSIVE STEP!
     opt.step()
     return loss
