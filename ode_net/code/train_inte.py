bab79830abe3248735b7ada848ffc6b2ce73046c
Intekhab Hossain <ihossain@g.harvard.edu>
2021-04-25 20:37:19 +0000

    model fits on full dataset

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -125,7 +125,7 @@ def save_model(odenet, folder, filename):
 
 parser = argparse.ArgumentParser('Testing')
 parser.add_argument('--settings', type=str, default='config_inte.cfg')
-clean_name = "chalmers_350genes_100samples_earlyT_0noise_0bimod_0pt1initvar"
+clean_name = "chalmers_690genes_100samples_earlyT_0noise_0bimod_0pt1initvar"
 #parser.add_argument('--data', type=str, default='C:/STUDIES/RESEARCH/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 parser.add_argument('--data', type=str, default='/home/ubuntu/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 
@@ -351,7 +351,7 @@ if __name__ == "__main__":
             decrease_lr(opt, settings['verbose'])
         
         #Decrease learning rate as a one-time thing:
-        if train_loss < 7*10**(-4) and first_drop_done == False:
+        if (train_loss < 7*10**(-4) and first_drop_done == False) or (epoch == 60 and first_drop_done == False):
             decrease_lr(opt, settings['verbose'], one_time_drop= 1*10**(-3))
             first_drop_done = True
         
