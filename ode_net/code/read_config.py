c106cf0361a6ed1a56abc1c4c609fe75c7b1f343
Intekhab Hossain <ihossain@g.harvard.edu>
2021-07-17 18:45:33 +0000

    moved to _old

--- ode_net/code/read_config.py
+++ ode_net/code/read_config.py
@@ -44,9 +44,9 @@ def _convert_arguments(settings):
     converted_settings['explicit_time'] = settings.getboolean('explicit_time')
     converted_settings['relative_error'] = False
 
-    converted_settings['pretrained_model'] = settings.getboolean('pretrained_model') 
-    converted_settings['lr_range_test'] = settings.getboolean('lr_range_test') 
-    converted_settings['scale_expression'] = settings.getfloat('scale_expression')
+    converted_settings['pretrained_model'] = False 
+    converted_settings['lr_range_test'] = False 
+    converted_settings['scale_expression'] = 1
     converted_settings['log_scale'] = settings['log_scale']
     converted_settings['init_bias_y'] = 0
     
