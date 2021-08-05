225dd330db974a2f595a05e0deb312b5aa3a30c6
Intekhab Hossain <ihossain@g.harvard.edu>
2021-08-05 23:09:57 +0000

    made yeast 6T dataset

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -132,9 +132,9 @@ def save_model(odenet, folder, filename):
 
 parser = argparse.ArgumentParser('Testing')
 parser.add_argument('--settings', type=str, default='config_inte.cfg')
-clean_name = "mouse_SC_1010genes_72samples"
+clean_name = "y5_384genes_6T"
 #parser.add_argument('--data', type=str, default='C:/STUDIES/RESEARCH/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
-parser.add_argument('--data', type=str, default='/home/ubuntu/neural_ODE/mouse_single_cell_data/clean_data/{}.csv'.format(clean_name))
+parser.add_argument('--data', type=str, default='/home/ubuntu/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 
 args = parser.parse_args()
 
