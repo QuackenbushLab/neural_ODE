1b7e5bbfeb205f749982c6605f87d22cb3f58737
Intekhab Hossain <ihossain@g.harvard.edu>
2021-08-11 05:02:58 +0000

    moved items to _old

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -132,7 +132,7 @@ def save_model(odenet, folder, filename):
 
 parser = argparse.ArgumentParser('Testing')
 parser.add_argument('--settings', type=str, default='config_inte.cfg')
-clean_name = "mouse_SC_1010genes_5highvarprcntlsamples"
+clean_name = "mouse_SC_1010genes_5cellordersamples"
 #parser.add_argument('--data', type=str, default='C:/STUDIES/RESEARCH/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 parser.add_argument('--data', type=str, default='/home/ubuntu/neural_ODE/mouse_single_cell_data/clean_data/{}.csv'.format(clean_name))
 
