2a4660b1884c9adc949acf47d4b15c2b5de8d921
Intekhab Hossain <ihossain@g.harvard.edu>
2021-08-12 20:22:20 +0000

    made changes to inspect_model.py

--- ode_net/code/inspect_model.py
+++ ode_net/code/inspect_model.py
@@ -26,100 +26,38 @@ from visualization_inte import *
 import matplotlib.pyplot as plt
 
 torch.set_num_threads(4) #CHANGE THIS!
-
-
-def _build_save_file_name(save_path, epochs):
-    return '{}-{}-{}({};{})_{}_{}epochs'.format(str(datetime.now().year), str(datetime.now().month),
-        str(datetime.now().day), str(datetime.now().hour), str(datetime.now().minute), save_path, epochs)
-
-parser = argparse.ArgumentParser('Testing')
-parser.add_argument('--settings', type=str, default='config_inte.cfg')
-clean_name = "chalmers_350genes_150samples_earlyT_0bimod_1initvar"
-#parser.add_argument('--data', type=str, default='C:/STUDIES/RESEARCH/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
-parser.add_argument('--data', type=str, default='/home/ubuntu/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
-
-args = parser.parse_args()
-
-# Main function
-if __name__ == "__main__":
-    print('Setting recursion limit to 3000')
-    sys.setrecursionlimit(3000)
-    print('Loading settings from file {}'.format(args.settings))
-    settings = read_arguments_from_file(args.settings)
-    cleaned_file_name = clean_name
-    save_file_name = _build_save_file_name(cleaned_file_name, settings['epochs'])
-
-    if settings['debug']:
-        print("********************IN DEBUG MODE!********************")
-        save_file_name= '(DEBUG)' + save_file_name
-    output_root_dir = '{}/{}/'.format(settings['output_dir'], save_file_name)
-
-    img_save_dir = '{}img/'.format(output_root_dir)
-    #intermediate_models_dir = '{}intermediate_models/'.format(output_root_dir)
-
-    # Create image and model save directory
-    if not os.path.exists(output_root_dir):
-        os.makedirs(output_root_dir, exist_ok=True)
-    if not os.path.exists(img_save_dir):
-        os.mkdir(img_save_dir)
-    #if not os.path.exists(intermediate_models_dir):
-    #    os.mkdir(intermediate_models_dir)
-
-    # Save the settings for future reference
-    with open('{}/settings.csv'.format(output_root_dir), 'w') as f:
-        f.write("Setting,Value\n")
-        for key in settings.keys():
-            f.write("{},{}\n".format(key,settings[key]))
-
-    # Use GPU if available
-    if not settings['cpu']:
-        os.environ["CUDA_VISIBLE_DEVICES"]="0"
-        print("Trying to run on GPU -- cuda available: " + str(torch.cuda.is_available()))
-        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        print("Running on", device)
-        #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    else:
-        print("Running on CPU")
-        device = 'cpu'
     
-    data_handler = DataHandler.fromcsv(args.data, device, settings['val_split'], normalize=settings['normalize_data'], 
-                                        batch_type=settings['batch_type'], batch_time=settings['batch_time'], 
-                                        batch_time_frac=settings['batch_time_frac'],
-                                        noise = settings['noise'],
-                                        img_save_dir = img_save_dir,
-                                        scale_expression = settings['scale_expression'],
-                                        log_scale = settings['log_scale'],
-                                        init_bias_y = settings['init_bias_y'])
+sums_model = torch.load('/home/ubuntu/neural_ODE/ode_net/code/output/_pretrained_best_model/best_val_model_sums.pt')
+alpha = torch.load('/home/ubuntu/neural_ODE/ode_net/code/output/_pretrained_best_model/best_val_model_model_weights.pt')
+gene_mult = torch.load('/home/ubuntu/neural_ODE/ode_net/code/output/_pretrained_best_model/best_val_model_gene_multipliers.pt')
 
-    # Initialization
-    odenet = ODENet(device, data_handler.dim, explicit_time=settings['explicit_time'], neurons = settings['neurons_per_layer'], 
-                    log_scale = settings['log_scale'], init_bias_y = settings['init_bias_y'])
-    odenet.float()
-    param_count = sum(p.numel() for p in odenet.parameters() if p.requires_grad)
-    param_ratio = round(param_count/ (data_handler.dim)**2, 3)
-    print("Using a NN with {} neurons per layer, with {} trainable parameters, i.e. parametrization ratio = {}".format(settings['neurons_per_layer'], param_count, param_ratio))
-    
-    pretrained_model_file = '/home/ubuntu/neural_ODE/ode_net/code/output/_pretrained_best_model/best_train_model.pt'.format(settings['output_dir'])
-    odenet.load(pretrained_model_file)
-    
-    plt.clf()
-    fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2, sharex = True)
-    
-    ax0.hist(odenet.net_sums.linear_1.weight.detach().numpy().flatten(), bins = 100)
-    ax0.set_title('first layer init')
+W1 = np.transpose(sums_model.linear_1.weight.detach().numpy())
+B1 = np.transpose(sums_model.linear_1.bias.detach().numpy())
+Wo = np.transpose(sums_model.linear_out.weight.detach().numpy())
+Bo = np.transpose(sums_model.linear_out.bias.detach().numpy())
+alpha = np.transpose(torch.sigmoid(alpha.detach()).numpy())
+gene_mult = np.transpose(torch.relu(gene_mult.detach()).numpy())
 
-    ax1.hist(odenet.net_sums.linear_out.weight.detach().numpy().flatten(), bins = 100)
-    ax1.set_title('out layer init')
 
-    ax2.hist(odenet.net.linear_1.weight.detach().numpy().flatten(), bins = 100)
-    ax2.set_title('first layer FINAL')
-    
-    ax3.hist(odenet.net.linear_out.weight.detach().numpy().flatten(), bins = 100)
-    ax3.set_title('out layer FINAL')
+np.savetxt("/home/ubuntu/neural_ODE/ode_net/code/model_inspect/w1.csv", W1, delimiter=",")
+np.savetxt("/home/ubuntu/neural_ODE/ode_net/code/model_inspect/b1.csv", B1, delimiter=",")
+np.savetxt("/home/ubuntu/neural_ODE/ode_net/code/model_inspect/wo.csv", Wo, delimiter=",")
+np.savetxt("/home/ubuntu/neural_ODE/ode_net/code/model_inspect/bo.csv", Bo, delimiter=",")
+np.savetxt("/home/ubuntu/neural_ODE/ode_net/code/model_inspect/alpha.csv", alpha, delimiter=",")
+np.savetxt("/home/ubuntu/neural_ODE/ode_net/code/model_inspect/gene_mult.csv", gene_mult, delimiter=",")
 
-    ax0.set_xlim(-0.5, 0.5)
-    ax1.set_xlim(-0.5, 0.5)
-    ax2.set_xlim(-0.5, 0.5)
-    ax3.set_xlim(-0.5, 0.5)
 
-    plt.savefig('/home/ubuntu/neural_ODE/ode_net/code/output/_pretrained_best_model/viz_NN_weights.png')
+
+#plt.clf()
+#fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, sharex = True)
+
+#ax0.hist(sums_model.linear_1.weight.detach().numpy().flatten(), bins = 100)
+#ax0.set_title('first layer init')
+
+#ax1.hist(sums_model.linear_out.weight.detach().numpy().flatten(), bins = 100)
+#ax1.set_title('out layer init')
+
+#ax0.set_xlim(-0.2, 0.2)
+#ax1.set_xlim(-0.2, 0.2)
+
+#plt.savefig('/home/ubuntu/neural_ODE/ode_net/code/output/_pretrained_best_model/viz_NN_weights.png')
