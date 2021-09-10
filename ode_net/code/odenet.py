015057f619b429b93ff671e9fb0393beb2e348f0
Intekhab Hossain <ihossain@g.harvard.edu>
2021-09-10 02:43:40 +0000

    yeast y5 dampening experiments

--- ode_net/code/odenet.py
+++ ode_net/code/odenet.py
@@ -61,7 +61,7 @@ class LogShiftedSoftSignMod(nn.Module):
         super().__init__() # init the base class
 
     def forward(self, input):
-        shifted_input =  input -0.5 #need to figure out the shift
+        shifted_input =  input - 0.5 
         abs_shifted_input = torch.abs(shifted_input)
         soft_sign_mod = shifted_input/(1+abs_shifted_input)
         return(torch.log1p(soft_sign_mod))  
