850fa16b9a0ae1d8b8b0751916f4f9b05cf9a201
Intekhab Hossain <ihossain@g.harvard.edu>
2021-09-28 05:12:35 +0000

    good diff dyn results

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -146,7 +146,7 @@ def save_model(odenet, folder, filename):
 
 parser = argparse.ArgumentParser('Testing')
 parser.add_argument('--settings', type=str, default='config_inte.cfg')
-clean_name = "chalmers_350genes_150samples_earlyT_0bimod_1initvar"
+clean_name = "chalmers_350genes_target_150samples_earlyT_0bimod_1initvar"
 #parser.add_argument('--data', type=str, default='C:/STUDIES/RESEARCH/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 parser.add_argument('--data', type=str, default='/home/ubuntu/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 
@@ -240,7 +240,7 @@ if __name__ == "__main__":
                 {'params': odenet.net_prods.linear_out.weight},
                 {'params': odenet.net_prods.linear_out.bias},
                 {'params': odenet.net_alpha_combine.linear_out.weight},
-                {'params': odenet.gene_multipliers,'lr': 1*settings['init_lr']}
+                {'params': odenet.gene_multipliers,'lr': 5*settings['init_lr']}
             ],  lr=settings['init_lr'], weight_decay=settings['weight_decay'])
 
 
