ed5d664c921eea2be6d30ef2d045f625a196283f
Intekhab Hossain <ihossain@g.harvard.edu>
2021-10-06 20:34:46 +0000

    moved files to _old

--- ode_net/code/odenet.py
+++ ode_net/code/odenet.py
@@ -22,7 +22,7 @@ class SoftsignMod(nn.Module):
         #self.shift = shift
 
     def forward(self, input):
-        shifted_input = input -0.5
+        shifted_input = torch.exp(input) -1
         abs_shifted_input = torch.abs(shifted_input)
         return(shifted_input/(1+abs_shifted_input))  
 
@@ -31,7 +31,7 @@ class LogShiftedSoftSignMod(nn.Module):
         super().__init__() # init the base class
 
     def forward(self, input):
-        shifted_input =  input -0.5
+        shifted_input =  torch.exp(input) -1
         abs_shifted_input = torch.abs(shifted_input)
         soft_sign_mod = shifted_input/(1+abs_shifted_input)
         return(torch.log1p(soft_sign_mod))  
@@ -66,9 +66,9 @@ class ODENet(nn.Module):
             )
         else: #6 layers
            
-            self.net_prods = nn.Sequential()
-            self.net_prods.add_module('activation_0',  LogShiftedSoftSignMod()) #
-            self.net_prods.add_module('linear_out', nn.Linear(ndim, neurons, bias = True))
+            #self.net_prods = nn.Sequential()
+            #self.net_prods.add_module('activation_0',  LogShiftedSoftSignMod()) #
+            #self.net_prods.add_module('linear_out', nn.Linear(ndim, neurons, bias = True))
             
             self.net_sums = nn.Sequential()
             self.net_sums.add_module('activation_0', SoftsignMod())
@@ -77,7 +77,10 @@ class ODENet(nn.Module):
             self.net_alpha_combine = nn.Sequential()
             self.net_alpha_combine.add_module('linear_out',nn.Linear(neurons, ndim, bias = False))
           
-            self.gene_multipliers = nn.Parameter(torch.rand(1,ndim, requires_grad= True))
+            #self.gene_multipliers = nn.Parameter(torch.rand(1,ndim, requires_grad= True))
+            #self.gene_taus = nn.Parameter(torch.randn(1,ndim, requires_grad= True))
+            #print("tau_mean =", torch.mean(torch.sigmoid(self.gene_taus)))
+            
             #self.minus_effect_factor = nn.Parameter(torch.zeros(1)+3, requires_grad= False) 
                 
         # Initialize the layers of the model
@@ -86,9 +89,9 @@ class ODENet(nn.Module):
                 nn.init.orthogonal_(n.weight, gain = calculate_gain("sigmoid"))
                 #nn.init.sparse_(n.weight,  sparsity=0.95, std = 0.05)   #0.05  
 
-        for n in self.net_prods.modules():
-            if isinstance(n, nn.Linear):
-                nn.init.sparse_(n.weight,  sparsity=0.95, std = 0.05) #0.05
+        #for n in self.net_prods.modules():
+        #    if isinstance(n, nn.Linear):
+        #        nn.init.sparse_(n.weight,  sparsity=0.95, std = 0.05) #0.05
                 #nn.init.orthogonal_(n.weight, gain = calculate_gain("sigmoid"))
                 
         for n in self.net_alpha_combine.modules():
@@ -108,9 +111,9 @@ class ODENet(nn.Module):
         #self.net_sums.linear_out.weight.register_hook(get_zero_grad_hook(mask_sums)) 
 
         
-        self.net_prods.to(device)
-        self.gene_multipliers.to(device)
-        #self.model_weights.to(device)
+        #self.net_prods.to(device)
+        #self.gene_multipliers.to(device)
+        #self.gene_taus.to(device)
         self.net_sums.to(device)
         self.net_alpha_combine.to(device)
         #self.minus_effect_factor.to(device)
@@ -127,8 +130,8 @@ class ODENet(nn.Module):
         #joint = self.net_alpha_combine(sums_prods_concat)
         #joint_relu = torch.relu(joint + 5) - 5
         joint = self.net_alpha_combine(sums)
-        #final = torch.relu(self.gene_multipliers)*(joint ) 
-        final = joint - y
+        #final = torch.relu(self.gene_multipliers)*(joint -y) 
+        final = joint #- y*torch.sigmoid(self.gene_taus)
         return(final) 
 
     def save(self, fp):
@@ -139,12 +142,12 @@ class ODENet(nn.Module):
         
         prod_path =  fp[:idx] + '_prods' + fp[idx:]
         sum_path = fp[:idx] + '_sums' + fp[idx:]
-        #model_weight_path = fp[:idx] + '_model_weights' + fp[idx:]
-        torch.save(self.net_prods, prod_path)
+        model_tau_path = fp[:idx] + '_gene_taus' + fp[idx:]
+        #torch.save(self.net_prods, prod_path)
         torch.save(self.net_sums, sum_path)
         torch.save(self.net_alpha_combine, alpha_comb_path)
-        torch.save(self.gene_multipliers, gene_mult_path)
-        #torch.save(self.model_weights, model_weight_path)
+        #torch.save(self.gene_multipliers, gene_mult_path)
+        #torch.save(self.gene_taus, model_tau_path)
         
 
     def load_dict(self, fp):
