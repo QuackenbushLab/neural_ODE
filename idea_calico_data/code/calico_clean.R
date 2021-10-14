f077c7261b6d78887ab02b51d242699d3106abca
Intekhab Hossain <59975468+intekhab8@users.noreply.github.com>
2021-10-14 02:13:52 -0400

    1135 genes 20 sample (high var samples)

--- idea_calico_data/code/calico_clean.R
+++ idea_calico_data/code/calico_clean.R
@@ -78,7 +78,7 @@ small_data[, .N, by = sample_id][order(-N)] #makes sense 6175 gene * 8 t = 49400
 high_var_data <- full_data[reg_changed %in%  regs_with_exprs_data &
                              GENE %in% genes_to_keep_most_var,,]
 high_var_data[, .N, by = sample_id][order(-N)] #makes sense 6175 gene * 8 t = 49400 per sample
-most_var_samp <- high_var_data[, sd(exprs_ratio), by = sample_id][order(-V1)][1:3, sample_id] #makes sense 6175 gene * 8 t = 49400 per sample
+most_var_samp <- high_var_data[, sd(exprs_ratio), by = sample_id][order(-V1)][1:20, sample_id] #makes sense 6175 gene * 8 t = 49400 per sample
 
 
 datamat <- dcast(full_data,
@@ -93,13 +93,14 @@ datamat_high_var <- dcast(high_var_data,
                           sample_id + reg_changed + GENE ~ time,
                           value.var = "exprs_ratio")
 
-datamat_high_var_1samp <- dcast(high_var_data[sample_id == most_var_samp[1]],
+datamat_high_var_20samp <- dcast(high_var_data[sample_id %in% most_var_samp[1:20]],
                           sample_id + reg_changed + GENE ~ time,
                           value.var = "exprs_ratio")
 
 datamat_high_var_3samp <- dcast(high_var_data[sample_id %in% most_var_samp[1:3]],
                                 sample_id + reg_changed + GENE ~ time,
                                 value.var = "exprs_ratio")
+
 
 
 
@@ -117,7 +118,7 @@ setnames(datamat_high_var,
          new = time_points
 )
 
-setnames(datamat_high_var_1samp, 
+setnames(datamat_high_var_20samp, 
          old = c("0.0", "05.0","10.0","15.0","20.0",
                  "30.0","45.0","90.0"),
          new = time_points
@@ -160,16 +161,16 @@ top_row[[2]] <- high_var_data[, length(unique(sample_id))]
 datamat_high_var <- rbind(top_row, datamat_high_var)
 
 
-datamat_high_var_1samp <- datamat_high_var_1samp[, .SD[1:(.N+1)], 
+datamat_high_var_20samp <- datamat_high_var_20samp[, .SD[1:(.N+1)], 
                                      by=.(sample_id, reg_changed)][is.na(GENE), 
                                                                    (time_points) := as.list(time_vals)]
-datamat_high_var_1samp[,c("sample_id","reg_changed","GENE") := NULL]
-datamat_high_var_1samp[, c("time_0", "time_5"):= NULL]
+datamat_high_var_20samp[,c("sample_id","reg_changed","GENE") := NULL]
+datamat_high_var_20samp[, c("time_0", "time_5"):= NULL]
 
 top_row <- as.list(rep(NA, length(time_vals)-2))
 top_row[[1]] <- length(genes_to_keep_most_var)
-top_row[[2]] <- 1
-datamat_high_var_1samp <- rbind(top_row, datamat_high_var_1samp)
+top_row[[2]] <- 20
+datamat_high_var_20samp <- rbind(top_row, datamat_high_var_20samp)
 
 
 
@@ -233,8 +234,8 @@ write.table( datamat_high_var,
              col.names = FALSE,
              na = "")
 
-write.table( datamat_high_var_1samp,
-             "C:/STUDIES/RESEARCH/neural_ODE/idea_calico_data/clean_data/calico_1135highvargenes_1sample_6T.csv", 
+write.table( datamat_high_var_20samp,
+             "C:/STUDIES/RESEARCH/neural_ODE/idea_calico_data/clean_data/calico_1135highvargenes_20sample_6T.csv", 
              sep=",",
              row.names = FALSE,
              col.names = FALSE,
