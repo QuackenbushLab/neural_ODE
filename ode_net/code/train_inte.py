dedc39bf691928994f4cf6d557d6a0b7bd758dda
Intekhab Hossain <ihossain@g.harvard.edu>
2021-10-25 02:59:07 +0000

    files deleted

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -139,7 +139,7 @@ def save_model(odenet, folder, filename):
 
 parser = argparse.ArgumentParser('Testing')
 parser.add_argument('--settings', type=str, default='config_inte.cfg')
-clean_name =  "calico_1135highvargenes_50samples_6T" #"y5_384genes_17T"
+clean_name =  "calico_6175genes_20highvarsamples_6T" #"y5_384genes_17T"
 #parser.add_argument('--data', type=str, default='C:/STUDIES/RESEARCH/neural_ODE/ground_truth_simulator/clean_data/{}.csv'.format(clean_name))
 parser.add_argument('--data', type=str, default='/home/ubuntu/neural_ODE/idea_calico_data/clean_data/{}.csv'.format(clean_name))
 
