9cb7041b2ed081a9c6fb885cd4754c63bf27c856
Intekhab Hossain <ihossain@g.harvard.edu>
2022-04-15 18:47:27 -0400

    made vert plot bargraph

--- ode_net/code/extra_modules/fig_ootb_mse_old.py
+++ ode_net/code/extra_modules/fig_ootb_mse_old.py
@@ -16,8 +16,7 @@ if __name__ == "__main__":
     
     neuron_dict = {"sim350": 40, "sim690": 50}
     num_gene_dict = {"sim350": 350, "sim690": 690}
-    models = ["phoenix","phoenix_noprior", "ootb_tanh",
-                "ootb_relu","ootb_sigmoid"]
+    models = ["ootb_sigmoid", "ootb_relu","ootb_tanh", "phoenix_noprior", "phoenix" ]
     datasets = ["sim350", "sim690"]
     noises = [0, 0.1]
     perf_info = {}
@@ -32,7 +31,7 @@ if __name__ == "__main__":
                     "ootb_sigmoid" : "OOTB NeuralODE (sigmoid)"} 
 
     model_hatch = {"phoenix":"", "phoenix_noprior" :"", 
-                    "ootb_tanh" : "//", "ootb_relu" : "..", "ootb_sigmoid" : "xx"}                
+                    "ootb_tanh" : "///", "ootb_relu" : "..", "ootb_sigmoid" : "---"}                
     
     leg_general_info = [Patch(facecolor=model_colors[this_model], edgecolor= "black", 
                             alpha = 0.5, hatch = model_hatch[this_model],
@@ -55,49 +54,50 @@ if __name__ == "__main__":
                 perf_info[line['dataset'].lower()][line['model']][float(line['noise'])][this_metric] = float(line[this_metric])
 
     #Plotting setup
-    fig_ootb_mse = plt.figure(figsize=(13,6))
-    #plt.grid(visible = True)
-    axes_ootb_mse = fig_ootb_mse.subplots(ncols= len(datasets),nrows = 1,  
-    sharex=False, sharey=False, 
+    fig_ootb_mse = plt.figure(figsize=(6,8))
+    #plt.grid(True)
+    axes_ootb_mse = fig_ootb_mse.subplots(nrows= len(datasets),ncols = 1,  
+    sharex=False, sharey=True, 
     subplot_kw={'frameon':True})
     #fig_ootb_mse.subplots_adjust(hspace=0.0, wspace=0.0)
     border_width = 1.5
     tick_lab_size = 11
-    ax_lab_size = 15
     ind = np.arange(len(noises))  # the x locations for the groups
-    height= 0.15  # the width of the bars
+    width = 0.15  # the width of the bars
     deltas = [-2, -1, 0, 1, 2]
     print("......")
     
     for this_data in datasets:
         print("Now on data = {}".format(this_data))
         for this_model in models:
-            col_num = datasets.index(this_data)
-            ax = axes_ootb_mse[col_num]
+            #print("Now on data = {}, noise = {}".format(this_data, this_noise))
+            
+            row_num = datasets.index(this_data)
+            ax = axes_ootb_mse[row_num]
             ax.spines['bottom'].set_linewidth(border_width)
             ax.spines['left'].set_linewidth(border_width)
             ax.spines['top'].set_linewidth(border_width)
             ax.spines['right'].set_linewidth(border_width)
-            ax.set_xlim((6*10**-4,5*10**-3))
-            ax.set_yticks(ind)
-            ax.set_yticklabels(['No noise', 'High noise'], rotation=90, va = "center")
             ax.tick_params(axis='x', labelsize= tick_lab_size)
             ax.tick_params(axis='y', labelsize= tick_lab_size)
+            ax.set_ylim((6*10**-4,5*10**-3))
+            ax.set_xticks(ind)
+            ax.set_xticklabels(['No noise', 'High noise'])
             
             this_delta = deltas[models.index(this_model)] 
             this_model_mses =  [perf_info[this_data][this_model][this_noise]['true_val_MSE'] for this_noise in noises]
-            ax.barh(ind + height*this_delta, this_model_mses, height = height,
+            ax.bar(ind + width*this_delta, this_model_mses, width = width, 
                     color = model_colors[this_model], alpha = 0.5,  edgecolor = "black", 
                     linewidth = 1.5, align = 'center', hatch = model_hatch[this_model])
-            ax.set_xscale("log")
-            #ax.set_axisbelow(True)
-            ax.grid(visible = True, which = "both", axis = "x", color = "black", 
-            linestyle = "--", alpha = 0.3, zorder = 3)
-            
-        ax.set_xlabel('validation MSE ({})'.format(this_data.upper()), fontsize=ax_lab_size)
-        if col_num == 0:
-            ax.set_ylabel('Noise level', fontsize=ax_lab_size)
-    fig_ootb_mse.legend(handles = leg_general_info, loc='upper center', prop={'size': 12}, 
-                        ncol = 3,  handleheight=1.5, frameon = False)
+            #ax.cla()
+        ax.set_ylabel('validation MSE ({})'.format(this_data.upper()), fontsize=15)
+        if row_num == 1:
+            ax.set_xlabel('Noise level', fontsize=15)
+            #if row_num == 0:
+            #    this_title = "Noise level = {}".format(this_noise) 
+            #    ax.set_title(this_title, fontsize = ax_lab_size, pad = 15)    
+    plt.yscale("log")
+    fig_ootb_mse.legend(handles = leg_general_info, loc='upper center', prop={'size': 11}, 
+                        ncol = 2,  handleheight=1.5)
     fig_ootb_mse.savefig('{}/manuscript_fig_ootb_mse.png'.format(output_root_dir), bbox_inches='tight')
     
