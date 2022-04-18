9a1ab5d5de1e986ae748e273969c691fb0c7dbba
Intekhab Hossain <ihossain@g.harvard.edu>
2022-04-18 11:12:15 +0000

    prior_0.5 yeast done

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -171,11 +171,11 @@ def training_step(odenet, data_handler, opt, method, batch_size, explicit_time, 
     pred_grad = odenet.prior_only_forward(t,batch_for_prior)
     loss_prior = torch.mean((pred_grad - prior_grad)**2)
     
-    loss_lambda = 0.8 #0.95
+    loss_lambda = 0.5 #0.95
     composed_loss = loss_lambda * loss_data + (1- loss_lambda) * loss_prior
     composed_loss.backward() #MOST EXPENSIVE STEP!
     opt.step()
-    return [loss_data, loss_data - loss_data]
+    return [loss_data, loss_prior]
 
 def _build_save_file_name(save_path, epochs):
     return '{}-{}-{}({};{})_{}_{}epochs'.format(str(datetime.now().year), str(datetime.now().month),
