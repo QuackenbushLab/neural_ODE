53243c57ce702ff0f46067a6171d18c7edace312
Intekhab Hossain <ihossain@g.harvard.edu>
2022-06-27 05:21:59 +0000

    0.999 helps for BC1000

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -171,7 +171,7 @@ def training_step(odenet, data_handler, opt, method, batch_size, explicit_time, 
     pred_grad = odenet.prior_only_forward(t,batch_for_prior)
     loss_prior = torch.mean((pred_grad - prior_grad)**2)
     
-    loss_lambda = 0.95 #0.9995
+    loss_lambda = 0.9999 #0.9995
     composed_loss = loss_lambda * loss_data + (1- loss_lambda) * loss_prior
     composed_loss.backward() #MOST EXPENSIVE STEP!
     opt.step()
