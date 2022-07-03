ff1d49a328b7f909a8bb74dba26ec945a59c294f
Intekhab Hossain <ihossain@g.harvard.edu>
2022-07-03 17:39:19 -0400

    desmedt erpos 500 data, orior, val

--- breast_cancer_data/code/breast_cancer_highvar_and_priormat_er_pos.R
+++ breast_cancer_data/code/breast_cancer_highvar_and_priormat_er_pos.R
@@ -3,15 +3,14 @@ library(stringr)
 
 
 ### will want genes to also have prior info so...
-long_prior <- fread("C:/STUDIES/RESEARCH/ODE_project_local_old/breast_cancer_data/otter_clean_harmonized_full_prior.csv")
+long_prior <- fread("C:/STUDIES/RESEARCH/ODE_project_local_old/breast_cancer_data/otter_clean_harmonized_full_prior_er_pos.csv")
 all_prior_affiliated_genes <- unique(c(long_prior$from, long_prior$to))
 ### start cleaning
 
-full_data <- fread("C:/STUDIES/RESEARCH/ODE_project_local_old/breast_cancer_data/Data_smooth_allgenes.csv")
-gene_names <- fread("C:/STUDIES/RESEARCH/ODE_project_local_old/breast_cancer_data/GeneNames.csv",
-                    header = F)
-names(gene_names) <- "gene"
-full_data <- cbind(gene_names, full_data)
+full_data <- fread("C:/STUDIES/RESEARCH/ODE_project_local_old/breast_cancer_data/Breast cancer-ER subtypes/er=1/er1_Ordered_Data.csv")
+full_data <- full_data[-.N] #remove grade row
+setnames(full_data, "V1", "gene")
+gene_names <- full_data[, .(gene)]
 
 full_data <- full_data[!is.na(gene) & gene != "", ]
 
@@ -37,20 +36,22 @@ genes_names_to_save <- full_data[, .(gene)]
 full_data[, gene := NULL]
 full_data <- full_data/15 #NORMALIZE to 0-1
 
-time_vals <- fread("C:/STUDIES/RESEARCH/ODE_project_local_old/breast_cancer_data/LatantTime.csv")
+time_vals <- fread("C:/STUDIES/RESEARCH/ODE_project_local_old/breast_cancer_data/Breast cancer-ER subtypes/er=1/er1_Temporal_Progression.csv")
+names(time_vals) <- c("pid", "latent_time")
+time_val_row <- lapply(exprs_cols, function(x){10*time_vals[pid==x, latent_time]})
 
-full_data <- rbind(full_data, time_vals)
-top_row <- as.list(rep(NA, dim(time_vals)[2]))
+full_data <- rbind(full_data, time_val_row)
+top_row <- as.list(rep(NA, length(time_val_row)))
 top_row[[1]] <- num_genes
 top_row[[2]] <- 1
 full_data <- rbind(top_row, full_data)
 
 write.csv(genes_names_to_save , 
-          "C:/STUDIES/RESEARCH/neural_ODE/breast_cancer_data/clean_data/desmedt_gene_names_1000.csv",
+          "C:/STUDIES/RESEARCH/neural_ODE/breast_cancer_data/clean_data/desmedt_gene_names_erpos_500.csv",
           row.names = F)
 
 write.table(full_data,
-             "C:/STUDIES/RESEARCH/neural_ODE/breast_cancer_data/clean_data/desmedt_1000genes_1sample_186T.csv", 
+             "C:/STUDIES/RESEARCH/neural_ODE/breast_cancer_data/clean_data/desmedt_erpos_500genes_1sample_186T.csv", 
              sep=",",
              row.names = FALSE,
              col.names = FALSE,
@@ -83,7 +84,7 @@ edges[,
       by= .(from, to)]
 
 write.table(edge_mat,
-            "C:/STUDIES/RESEARCH/neural_ODE/breast_cancer_data/clean_data/edge_prior_matrix_desmedt_1000.csv",
+            "C:/STUDIES/RESEARCH/neural_ODE/breast_cancer_data/clean_data/edge_prior_matrix_desmedt_erpos_500.csv",
             sep = ",",
             row.names = F, 
             col.names = F
