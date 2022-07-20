385cac81a8e4f6674d50659004e910f7a4e3558a
Intekhab Hossain <ihossain@g.harvard.edu>
2022-07-20 17:05:40 +0000

    0.70 works better for desmedt4000

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -171,7 +171,7 @@ def training_step(odenet, data_handler, opt, method, batch_size, explicit_time, 
     pred_grad = odenet.prior_only_forward(t,batch_for_prior)
     loss_prior = torch.mean((pred_grad - prior_grad)**2)
     
-    loss_lambda = 0.80 #0.9995
+    loss_lambda = 0.60 #0.9995
     composed_loss = loss_lambda * loss_data + (1- loss_lambda) * loss_prior
     composed_loss.backward() #MOST EXPENSIVE STEP!
     opt.step()
@@ -246,7 +246,7 @@ if __name__ == "__main__":
     #Read in the prior matrix
     prior_mat_loc = '/home/ubuntu/neural_ODE/breast_cancer_data/clean_data/edge_prior_matrix_desmedt_4000.csv'
     prior_mat = read_prior_matrix(prior_mat_loc)
-    batch_for_prior = torch.rand(500,1,prior_mat.shape[0], device = data_handler.device)*2 - 1
+    batch_for_prior = torch.rand(500,1,prior_mat.shape[0], device = data_handler.device) - 0.5
     prior_grad = torch.matmul(batch_for_prior,prior_mat) #can be any model here that predicts the derivative
 
     # Initialization
@@ -292,7 +292,7 @@ if __name__ == "__main__":
 
 
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(opt, mode='min', 
-    factor=0.9, patience=3, threshold=1e-07, 
+    factor=0.9, patience=3, threshold=1e-08, 
     threshold_mode='abs', cooldown=0, min_lr=0, eps=1e-08, verbose=True)
 
     
