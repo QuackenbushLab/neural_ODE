f867d4d4997b5ef2f0ab188d4e4d19228b7dc562
Intekhab Hossain <ihossain@g.harvard.edu>
2022-07-25 02:19:25 +0000

    set up 11165 genes now

--- ode_net/code/train_inte.py
+++ ode_net/code/train_inte.py
@@ -253,7 +253,7 @@ if __name__ == "__main__":
     
     #Read in the prior matrix
     prior_mat_loc = '/home/ubuntu/neural_ODE/breast_cancer_data/clean_data/edge_prior_matrix_desmedt_11165.csv'
-    prior_mat = read_prior_matrix(prior_mat_loc)
+    prior_mat = read_prior_matrix(prior_mat_loc, sparse = True)
     batch_for_prior = torch.rand(10000,1,prior_mat.shape[0], device = data_handler.device) - 0.5
     prior_grad = torch.matmul(batch_for_prior,prior_mat) #can be any model here that predicts the derivative
     loss_lambda = 0.99
