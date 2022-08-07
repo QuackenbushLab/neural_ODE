1b6321c14311b9d529db4a5b46da6fa8139cbd09
intekhab8 <ihossain@g.harvard.edu>
2022-08-07 06:32:41 +0000

    noiseless tanh done

--- ode_net/code/train_inte_ootb.py
+++ ode_net/code/train_inte_ootb.py
@@ -24,7 +24,7 @@ from read_config import read_arguments_from_file
 from solve_eq import solve_eq
 from visualization_inte import *
 
-torch.set_num_threads(8) #CHANGE THIS!
+#torch.set_num_threads(8) #CHANGE THIS!
 
 def plot_LR_range_test(all_lrs_used, training_loss, img_save_dir):
     plt.figure()
