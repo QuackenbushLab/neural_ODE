59c6380a8dcba0357a7c3624a1d3f99da630cfed
Intekhab Hossain <ihossain@g.harvard.edu>
2022-08-10 19:00:52 -0400

    breast cancer plot updated

--- ode_net/code/fig_breast_cancer_res.py
+++ ode_net/code/fig_breast_cancer_res.py
@@ -33,32 +33,35 @@ if __name__ == "__main__":
     output_root_dir = '{}/{}/'.format("output", save_file_name)
     if not os.path.exists(output_root_dir):
         os.makedirs(output_root_dir, exist_ok=True)
-    
+
     neuron_dict = {"sim350": 40, "sim690": 50}
     #models = ["phoenix"]
     datasets = ["breast_cancer"]
-    
-    
+
+
     #Plotting setup
-    #plt.xticks(fontsize=10)
-    #plt.yticks(fontsize=10)
-    fig_breast_cancer = plt.figure(figsize=(10,18)) # tight_layout=True
-    axes_breast_cancer = fig_breast_cancer.subplots(ncols= 1, nrows=1, 
-    sharex=False, sharey=False, 
-    subplot_kw={'frameon':True})
-    #fig_breast_cancer.subplots_adjust(hspace=0, wspace=0)
+    fig_breast_cancer = plt.figure(figsize=(10,18), tight_layout=True)
+    gs = fig_breast_cancer.add_gridspec(10, 3, hspace = 0)
     border_width = 1.5
-    tick_lab_size = 12
+    tick_lab_size = 14
     ax_lab_size = 15
+    all_genes = [500, 2000, 4000, 11165]
     
+    print("......")
+
+    this_data = "breast_cancer"
+
+    ax = fig_breast_cancer.add_subplot(gs[1:10, :])
+    ax.spines['bottom'].set_linewidth(border_width)
+    ax.spines['left'].set_linewidth(border_width)
+    ax.spines['top'].set_linewidth(border_width)
+    ax.spines['right'].set_linewidth(border_width)
+    ax.cla()
+    ax.set_xlim(0,4)
+
+    print("making heatmap")
     z = np.loadtxt(open("C:/STUDIES/RESEARCH/neural_ODE/all_manuscript_models/breast_cancer/all_harm_cent_wide.csv", "rb"), 
-                    dtype = "str",delimiter=",", skiprows=1, usecols = (1,2,3,4))
-
-    gene_names = np.loadtxt(open("C:/STUDIES/RESEARCH/neural_ODE/all_manuscript_models/breast_cancer/all_harm_cent_wide.csv", "rb"), 
-                    dtype = "str",delimiter=",", skiprows=1, usecols = (0))
-
-    gene_names = np.char.strip(gene_names, '"')
-
+        dtype = "str",delimiter=",", skiprows=1, usecols = (1,2,3,4))
     num_tops = z.shape[0]
     print("The analysis contains", num_tops, "genes.")
     num_models = z.shape[1]
@@ -66,64 +69,103 @@ if __name__ == "__main__":
     z = z.astype(float)
     z[ z == 0] = np.nanmax(z)
     z = z.transpose()
-    y, x = np.meshgrid(np.linspace(0, num_tops, num_tops), np.linspace(0, num_models+1, num_models+1))
-                
-    plt.grid(True)
+    ind = np.arange(num_models) +0.5
     
-    print("......")
-    
-    for this_data in datasets:
-        #this_data_handler = datahandler_dict[this_data]
-                
-        ax = axes_breast_cancer#[row_num]
-        ax.spines['bottom'].set_linewidth(border_width)
-        ax.spines['left'].set_linewidth(border_width)
-        ax.spines['top'].set_linewidth(border_width)
-        ax.spines['right'].set_linewidth(border_width)
-        ax.cla()
+    gene_names = np.loadtxt(open("C:/STUDIES/RESEARCH/neural_ODE/all_manuscript_models/breast_cancer/all_harm_cent_wide.csv", "rb"), 
+        dtype = "str",delimiter=",", skiprows=1, usecols = (0))
+    gene_names = np.char.strip(gene_names, '"')
 
-        
-        
-        z_min, z_max = np.nanmin(z), np.nanmax(z)
-        c = ax.pcolormesh(z.transpose(), cmap='Blues_r', vmin=z_min, vmax=120, 
-                            norm = colors.PowerNorm(gamma = 0.4),
-                            shading = "nearest") 
-        #ax.axis([x.min(), x.max(), y.min(), y.max()]) 
-        
-        fig_breast_cancer.canvas.draw()
-        #labels_y = [item.get_text() for item in ax.get_yticklabels()]
-        #labels_x = [item.get_text() for item in ax.get_xticklabels()]
-        
-        ax.set_yticks(np.arange(num_tops)+0.5)    
-        ax.set_yticklabels(gene_names)
-        ax.tick_params(axis='x', labelsize= tick_lab_size)
-        ax.tick_params(axis='y', labelsize= tick_lab_size)
-        
-        for idx in range(num_tops):
-            plt.axhline(y=idx, xmin=0, xmax=4, linestyle='dotted', color = "gray", alpha = 0.3)
+    z_min, z_max = np.nanmin(z), np.nanmax(z)
+    c = ax.pcolormesh(z.transpose(), cmap='Blues_r', vmin=z_min, vmax=120, 
+            norm = colors.PowerNorm(gamma = 0.4),
+            shading = "nearest") 
+    #ax.axis([x.min(), x.max(), y.min(), y.max()]) 
 
-        for i in range(num_tops):
-            for j in range(num_models):
-                if z[j,i] <= 5:
-                    text = ax.text(j + 0.5, i + 0.5, int(z[j, i]),
-                                ha="center", va="center", color="w",
-                                size = 13, weight = "bold")
-                
-        #if row_num == 0:
-        #    ax.set_title(model_labels[this_model], fontsize=ax_lab_size, pad = 10)
-        #if col_num == 0:
-        #    ax.set_ylabel("Noise level = {:.0%}".format(this_noise/0.5), fontsize = ax_lab_size) 
+    #fig_breast_cancer.canvas.draw()
+    #labels_y = [item.get_text() for item in ax.get_yticklabels()]
+    #labels_x = [item.get_text() for item in ax.get_xticklabels()]
 
-                 
-    cbar =  fig_breast_cancer.colorbar(c, ax=axes_breast_cancer, 
-                                        shrink=0.5, orientation = "vertical", pad = 0.05,
-                                        extend = "max")
-    #cbar.set_ticks([0, 0.03, -0.03])
-    #cbar.set_ticklabels(['None', 'Activating', 'Repressive'])
-    #cbar.ax.tick_params(labelsize = tick_lab_size+3) 
+    ax.set_yticks(np.arange(num_tops)+0.5)    
+    ax.set_yticklabels(gene_names)
+    ax.tick_params(axis='y', labelsize= tick_lab_size)
+
+    for idx in range(num_tops):
+        ax.axhline(y=idx, xmin=0, xmax=num_models, linestyle='dotted', color = "black", alpha = 0.3)
+
+    for idx in range(num_models):
+        ax.axvline(x=idx, ymin=0, ymax=num_tops, linestyle='dotted', color = "black", alpha = 0.3)    
+
+    for i in range(num_tops):
+        for j in range(num_models):
+            if z[j,i] <= 5:
+                text = ax.text(j + 0.5, i + 0.5, int(z[j, i]),
+                            ha="center", va="center", color="w",
+                            size = 13, weight = "bold")
+
+    cbar =  fig_breast_cancer.colorbar(c, ax=ax, 
+                            shrink=0.7, orientation = "horizontal", pad = 0.05,
+                            extend = "max")
+    cbar.set_ticks([1, 5, 20, 60, 120])
+    cbar.set_ticklabels(['1', '5', '20', '60', '120 or more'])
+    cbar.ax.tick_params(labelsize = tick_lab_size) 
     #cbar.set_label(r'$\widetilde{D_{ij}}$= '+'Estimated effect of '+ r'$g_j$'+ ' on ' +r"$\frac{dg_i}{dt}$" +' in SIM350', size = ax_lab_size)
     cbar.outline.set_linewidth(2)
 
+    print("......")
+    print("overlaying performance metrics")
+    ax1 = fig_breast_cancer.add_subplot(gs[0, :], sharex = ax)
+    #ax1.spines['bottom'].set_linewidth(border_width)
+    #ax1.spines['left'].set_linewidth(border_width)
+    #ax1.spines['top'].set_linewidth(border_width)
+    #ax1.spines['right'].set_linewidth(border_width)
+    ax1.set_frame_on(False)
+    ax1.axes.get_xaxis().set_visible(False)
+    ax1.axes.get_yaxis().set_visible(False)
+    ax1.cla()
+    
+    perf_info = {}
+    metrics = ['var_explained', 'AUC','runtime_cost' ]
+    metric_cols = {'var_explained': 'green', 'AUC': 'orange','runtime_cost':'purple' }
+    metric_labels = {'var_explained': r'$R^2$', 'AUC': 'AUC','runtime_cost':'AWS ($)' }
+
+    leg_general_info = [Patch(facecolor=metric_cols[this_metric], edgecolor= "black", 
+                            alpha = 0.7, label= metric_labels[this_metric]) for this_metric in metrics]
+
+    for this_gene in all_genes:
+        if this_gene not in perf_info:
+            perf_info[this_gene] = {this_metric: 0 for this_metric in metrics}
+
+    perf_csv = csv.DictReader(open('C:/STUDIES/RESEARCH/neural_ODE/all_manuscript_models/breast_cancer/bc_model_perfs.csv', 'r'))
+    for line in perf_csv: 
+        for this_metric in metrics:
+            perf_info[float(line['num_gene'])][this_metric] = float(line[this_metric])
+
+    print(perf_info)
+
+    width= 0.17  # the width of the bars
+    deltas = [-1, 0, 1]
+    
+    for this_metric in metrics:
+        this_delta = deltas[metrics.index(this_metric)] 
+        this_perf_vals = [perf_info[this_gene][this_metric] for this_gene in all_genes]
+        ax1.bar(ind  + width*this_delta, this_perf_vals, width = width, 
+                        capsize = 5, color = metric_cols[this_metric], alpha = 0.7,  edgecolor = "black", 
+                        linewidth = 1.5, align = 'center')
+        
+    for this_metric in metrics:
+        this_delta = deltas[metrics.index(this_metric)] 
+        this_perf_vals = [perf_info[this_gene][this_metric] for this_gene in all_genes]
+        for this_idx in range(len(this_perf_vals)):
+            this_val = this_perf_vals[this_idx]
+            ax1.text(this_idx + 0.5 + width*this_delta, this_val + 0.1 , this_val, 
+                     ha="center", va="bottom", color="black",size = 15, rotation = 90)
+    
+    ax1.legend(handles = leg_general_info, loc='upper left', prop={'size': tick_lab_size}, 
+                        ncol = 1,  handleheight=1.5, frameon = False, bbox_to_anchor = (-0.15,1.2))
+
+    
+    ax.set_xticks(ind)    
+    ax.set_xticklabels(all_genes)
+    ax.tick_params(axis='x', labelsize= tick_lab_size + 3)
     
     fig_breast_cancer.savefig('{}/manuscript_fig_breast_cancer.png'.format(output_root_dir), bbox_inches='tight')
-    
