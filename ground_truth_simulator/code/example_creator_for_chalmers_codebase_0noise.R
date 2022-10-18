bb191da4794f9587c5b3cd91037bb1dcc428ec29
Intekhab Hossain <ihossain@g.harvard.edu>
2022-10-18 19:22:03 -0400

    prescient_350_inputs

--- ground_truth_simulator/code/example_creator_for_chalmers_codebase_0noise.R
+++ ground_truth_simulator/code/example_creator_for_chalmers_codebase_0noise.R
@@ -55,8 +55,8 @@ simseed = simseeds[102]
 
 #----1: sample network and create simulation----
 set.seed(simseed)
-#grnSmall = sampleGraph(grnFull, netSize, minTFs, seed = simseed)
-grnSmall = grnFull
+grnSmall = sampleGraph(grnFull, netSize, minTFs, seed = simseed)
+#grnSmall = grnFull
 
 
 if(edge_removal == T){
